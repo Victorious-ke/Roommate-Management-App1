1f6a2cd14049c6b19512b9d89ad1dc9da6b83397
LabanMugutu <labanmugutu@gmail.com>
2025-09-03 08:05:24 +0300

    Calles CalendarPage function

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -4,3 +4,14 @@ import "react-calendar/dist/Calendar.css";
 import { format } from "date-fns";
 import "./Calendar.css"; // custom styling
 
+function CalendarPage() {
+  const [date, setDate] = useState(new Date());
+  const [events, setEvents] = useState([]);
+  const [newEvent, setNewEvent] = useState({
+    title: "",
+    description: "",
+    startTime: "",
+    endTime: "",
+  });
+
+  
