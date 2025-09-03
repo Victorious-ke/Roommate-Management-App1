e731a96472e6697ea904a9ef342fa3e89974af04
LabanMugutu <labanmugutu@gmail.com>
2025-09-03 07:52:56 +0300

    Creates Calendar.jsx

---
+++ src/pages/Calendar.jsx
@@ -0,0 +1,6 @@
+import { useEffect, useState } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import { format } from "date-fns";
+import "./Calendar.css"; // custom styling
+
